a5b0a1fe0b631e751aca667043abe186577dc7df
son <nguyentruongsonpp@gmail.com>
2025-12-17 09:40:11 +0700

    Update

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.use(cookieParser());
 app.use(cors({
     origin: process.env.CLIENT_URL || "*",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }))
 app.use(express.static(path.join(__dirname, 'public')));
 
